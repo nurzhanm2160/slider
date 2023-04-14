abf661e9a12c4a3165dca7d8a42ee74f44434956
nurzhanm2160 <nurzhan.murzakhmet@gmail.com>
2023-04-14 19:35:58 +0600

    Модель для галлереи

--- src/gallery/models.py
+++ src/gallery/models.py
@@ -5,6 +5,22 @@ from django.db import models
 
 class Image(models.Model):
     image = models.ImageField('Изображение', upload_to='uploads/% Y/% m/% d/')
+    uploaded_time = models.DateTimeField('Дата добавления изображения', auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Изоброжение'
+        verbose_name_plural = 'Изоброжении'
 
     def __str__(self):
         return self.image
+
+
+class Gallery(models.Model):
+    images = models.ForeignKey(Image, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Галлерея'
+        verbose_name_plural = 'Галлерея'
+
+    def __str__(self):
+        return self.images
